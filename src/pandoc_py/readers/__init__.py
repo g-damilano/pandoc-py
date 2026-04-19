c6aa3bc414023a2de11f0f8352cc8a3bb09d8f93
Giacomo Damilano <104920835+g-damilano@users.noreply.github.com>
2026-04-19 10:11:07 +0100

    Export native reader from readers package

--- src/pandoc_py/readers/__init__.py
+++ src/pandoc_py/readers/__init__.py
@@ -1,15 +1,18 @@
 from .commonmark import CommonmarkScopeError, read_commonmark
 from .html import HtmlReaderError, read_html
 from .markdown import MarkdownScopeError, read_markdown
+from .native import NativeReaderError, read_native
 from .pandoc_json import PandocJsonReaderError, read_pandoc_json
 
 __all__ = [
     'CommonmarkScopeError',
     'HtmlReaderError',
     'MarkdownScopeError',
+    'NativeReaderError',
     'PandocJsonReaderError',
     'read_commonmark',
     'read_html',
     'read_markdown',
+    'read_native',
     'read_pandoc_json',
 ]
